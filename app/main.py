9f2d252e7c900468ab9ae92f8542d2bdf1b63f3c
bitz0416 <bitz940416@gmail.com>
2024-12-05 15:19:58 +0800

    Alter title to limit 100 chars and add due_date field

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker,Session
 
 app = FastAPI()
 
-DATABASE_URL = "sqlite:///./todos.db"
+DATABASE_URL = "sqlite:///./todos.dp"
 Base = declarative_base()
 engine = create_engine(DATABASE_URL,connect_args={"check_same_thread":False})
 SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
